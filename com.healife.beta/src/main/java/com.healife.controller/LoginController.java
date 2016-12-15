d093270a48632cc17bdd83232869156f96dd32dd
wilson <15800560165@163.com>
2016-12-15 15:49:03 +0800

    首次上传项目
    maven多模块项目

---
+++ com.healife.beta/src/main/java/com.healife.controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.healife.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by wilson on 2016/11/28.
+ */
+@Controller
+@RequestMapping("/heailfe")
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String toLogin(){
+
+
+        return "login";
+
+    }
+}
